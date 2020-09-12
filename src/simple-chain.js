2bf9ea4cae00ee2d5bbe633566c8640b55809f84
Egor <lalalayes2@gmail.com>
2020-09-12 15:27:50 +0300

    feat: complete chainmaker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain : [],
   getLength() {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
+    if(value === null || value === undefined)
+      {
+        this.chain.push(`( ${value} )`);
+      }
+    else
+      value === undefined ? this.chain.push(`( )`) : this.chain.push(`( ${value.toString()} )`);
+    return this;
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   removeLink(position) {
+    if (position > this.chain.length || position < 0 || isNaN(position)){
+      this.chain = [];
+      throw "Invalid position";
+    }
+    this.cahin = this.chain.splice(position - 1, 1);
+    return this;
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   reverseChain() {
+    this.chain = this.chain.reverse();
+    return this;
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let string = this.chain.join('~~');
+    this.chain = [];
+    return string;
+
   }
 };
 
