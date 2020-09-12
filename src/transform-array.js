9ad6678aac191df59bef99dd97f6c0207fc4770b
Egor <lalalayes2@gmail.com>
2020-09-12 15:26:57 +0300

    feat: complete array transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  // throw new CustomError('Not implemented');
+  if (!(arr instanceof Array))
+      throw "Not Array";
+    let res = [], size = arr.length;
+    let temp = 0;
+    let index;
+    if (size === 0)
+      return [];
+    for (let i = 0; i < size; i++)
+    {
+      switch (arr[i]) {
+        case `--discard-next`:
+          index = i + 1;
+            i++;
+            break;
+        case `--discard-prev`:
+          temp = res[i-1]
+          if(index !== i - 1)
+            res.pop();
+            break;
+        case `--double-next`:
+          temp = arr[i+1];
+            if(temp !== undefined)
+                res.push(temp);
+            break;
+        case `--double-prev`:
+            temp = res[res.length - 1]
+            if(temp !== undefined && index !== i - 1)
+                res.push(temp);
+            break;
+        default:
+            res.push(arr[i]);
+            break;
+    }
+    }
+    return res;
 };
